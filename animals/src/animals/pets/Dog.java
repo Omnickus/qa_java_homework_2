39c3bd55f8ead0c67f4992f3125e14399128bdd2
Omnic <moltushkina@bk.ru>
2025-09-03 22:33:46 +0300

    Animals

---
+++ animals/src/animals/pets/Dog.java
@@ -0,0 +1,11 @@
+package animals.pets;
+
+import animals.AbsAnimals;
+
+public class Dog extends AbsAnimals {
+
+    public Dog(String name, String color, Integer weight, Integer age) {
+        super(name, color, weight, age);
+    }
+
+}
---
+++ animals/src/interfaces/Flying.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface Flying {
+
+    public void fly();
+
+}
